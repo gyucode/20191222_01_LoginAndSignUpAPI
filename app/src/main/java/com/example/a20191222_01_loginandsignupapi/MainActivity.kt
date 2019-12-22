d883dc29e59ea0d81be05d4bb4007bb296173d58
gyucode <gyucode@gmail.com>
2019-12-22 15:47:16 +0900

    목록을 분석해서 => 데이터 객체로 변환 => 배열에 담아주기

--- app/src/main/java/com/example/a20191222_01_loginandsignupapi/MainActivity.kt
+++ app/src/main/java/com/example/a20191222_01_loginandsignupapi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a20191222_01_loginandsignupapi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.a20191222_01_loginandsignupapi.datas.BlackListData
 import com.example.a20191222_01_loginandsignupapi.datas.User
 import com.example.a20191222_01_loginandsignupapi.utils.ConnectServer
@@ -37,6 +38,35 @@ class MainActivity : BaseActivity() {
         ConnectServer.getRequestBlackList(mContext, object:ConnectServer.JsonResponseHandler{
             override fun onResponse(json: JSONObject) {
                 Log.d("블랙리스트 목록응답", json.toString())
+
+                val code = json.getInt("code")
+
+                if(code == 200){
+                    val data = json.getJSONObject("data")
+                    val black_lists = data.getJSONArray("black_lists")
+
+//                    블랙리스트 : 2개 => 0, 1
+                    for(i  in 0..black_lists.length()-1){
+////                        JSONArray 내부의 i 번째 JSONObject를 추출
+//                        val tempJson = black_lists.getJSONObject(i)
+//
+////                        뽑아낸 JSONObject => BlackListData클래스로 변경(리스트 뷰에서 활용가능)
+//                        val tempData = BlackListData.getBlackListDataFromjson(tempJson)
+//
+////                        BlackListData클래스로 변경된 변수를 ArrayList에 추가
+//                        blackList.add(tempData)
+
+                        blackList.add(BlackListData.getBlackListDataFromjson(black_lists.getJSONObject( i )) )
+
+                    }
+
+                }
+                else{
+                    val message = json.getString("message")
+                    runOnUiThread {
+                        Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         })
     }
