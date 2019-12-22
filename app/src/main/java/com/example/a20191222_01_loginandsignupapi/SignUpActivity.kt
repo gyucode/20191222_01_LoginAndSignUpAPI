1e88e5794c8e5ecd0f7371b264149478348dc480
gyucode <gyucode@gmail.com>
2019-12-22 11:20:41 +0900

    회원가입하기 작성

---
+++ app/src/main/java/com/example/a20191222_01_loginandsignupapi/SignUpActivity.kt
@@ -0,0 +1,20 @@
+package com.example.a20191222_01_loginandsignupapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValue()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValue() {
+    }
+
+}
