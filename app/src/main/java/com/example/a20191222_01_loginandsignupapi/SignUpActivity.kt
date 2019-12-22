49672cfd7f0312d58f4195cc2ab97ed30e4b2679
gyucode <gyucode@gmail.com>
2019-12-22 12:20:08 +0900

    API 호출 성공  - 응답처리 필요

--- app/src/main/java/com/example/a20191222_01_loginandsignupapi/SignUpActivity.kt
+++ app/src/main/java/com/example/a20191222_01_loginandsignupapi/SignUpActivity.kt
@@ -2,6 +2,10 @@ package com.example.a20191222_01_loginandsignupapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.a20191222_01_loginandsignupapi.utils.ConnectServer
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +14,29 @@ class SignUpActivity : BaseActivity() {
         setupEvents()
         setValue()
     }
+
     override fun setupEvents() {
+        signUpBtn.setOnClickListener {
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+            val inputName = nameEdt.text.toString()
+            val inputPhone = phonEdt.text.toString()
+
+            ConnectServer.putRequestSignUp(
+                mContext,
+                inputId,
+                inputPw,
+                inputName,
+                inputPhone,
+                object : ConnectServer.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+                        Log.d("회원가입응답",json.toString())
+
+                    }
+
+                })
+
+        }
 
     }
 
